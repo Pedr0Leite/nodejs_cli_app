b7b7db6a3d41e49dece21b491028475586a7cf7a
Pedr0Leite <48599344+Pedr0Leite@users.noreply.github.com>
2021-05-18 18:14:35 +0100

    clean logs

--- controllers/criptocoin.js
+++ controllers/criptocoin.js
@@ -1,28 +1,30 @@
-console.log('CRIPTO STARTS')
-const axios = require('axios');
-const dotenv = require('dotenv').config();
+console.log("CRIPTO STARTS");
+const axios = require("axios");
+const dotenv = require("dotenv").config();
 
 const coinKey = process.env.COIN_MARKET_KEY;
-console.log('coinKey :', coinKey);
 
+module.exports = async function getCripto() {
+  const url =
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+  const headersPart = {
+    "X-CMC_PRO_API_KEY": coinKey,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
 
-module.exports = async function getCripto(){
-    const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
-    const headersPart = {'X-CMC_PRO_API_KEY': coinKey, "Content-Type": "application/x-www-form-urlencoded"};
+  const finalArr = [];
 
-    const finalArr = [];
-
-    await axios({
-    method: 'get',
+  await axios({
+    method: "get",
     url: url,
     headers: headersPart,
     json: true,
-    gzip: true
-}).then(function (response) {
-    let cripto = response.data.data.filter(x=> x.symbol == process.env.CRIPTO)
+    gzip: true,
+  }).then(function (response) {
+    let cripto = response.data.data.filter(
+      (x) => x.symbol == process.env.CRIPTO
+    );
     finalArr.push(cripto);
-    });
-    // console.log('finalArr :', finalArr);
-    return finalArr;
-
+  });
+  return finalArr;
 };
